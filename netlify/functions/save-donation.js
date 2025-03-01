2967ec92ee80752653985b2a929e6d5ea13c6402
webakery <quadravision515@gmail.com>
2025-03-01 15:56:01 +0530

    Update save-donation.js

--- netlify/functions/save-donation.js
+++ netlify/functions/save-donation.js
@@ -1,1 +1,26 @@
 
+const fs = require("fs");
+const path = require("path");
+
+exports.handler = async (event) => {
+  if (event.httpMethod !== "POST") {
+    return { statusCode: 405, body: "Method Not Allowed" };
+  }
+
+  const filePath = path.join(__dirname, "../../public/donations.json");
+  const newDonation = JSON.parse(event.body);
+
+  try {
+    let existingData = [];
+    if (fs.existsSync(filePath)) {
+      existingData = JSON.parse(fs.readFileSync(filePath, "utf8"));
+    }
+    
+    existingData.push(newDonation);
+    fs.writeFileSync(filePath, JSON.stringify(existingData, null, 2));
+
+    return { statusCode: 200, body: JSON.stringify({ message: "Saved!" }) };
+  } catch (error) {
+    return { statusCode: 500, body: JSON.stringify({ error: "Failed to save data" }) };
+  }
+};
